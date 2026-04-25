98bb7e0b3c2eb8449bf8182bd354f61907d4ae46
ashik081 <freelancerashik81@gmail.com>
2026-04-25 22:22:07 +0600

    added navbar

---
+++ src/components/shared/BreakingNews.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+
+const BreakingNews = () => {
+  const news = [
+    {
+      _id: "1",
+      title: "Breaking News: Major Event Unfolds in the City",
+    },
+    {
+      _id: "2",
+      title: "Breaking News: New Policy Announced by the Government",
+    },
+    {
+      _id: "3",
+      title: "Breaking News: Sports Team Wins Championship",
+    },
+  ];
+  return (
+    <div className="container mx-auto">
+      <div className=" bg-gray-200 text-black py-2 px-4 flex items-center gap-4">
+        <button className="flex items-center justify-center  px-4 py-2 rounded-xl bg-white text-red-600 font-bold">
+          Latest
+        </button>
+        <Marquee pauseOnHover={true}>
+          {news.map((item) => (
+            <p key={item._id} className="px-8">
+              Breaking News: {item.title}
+            </p>
+          ))}
+        </Marquee>
+      </div>
+    </div>
+  );
+};
+
+export default BreakingNews;
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Avatar from "@/assets/user.png";
 
 const Navbar = () => {
-  return <div>navbar page</div>;
+  return (
+    <div className="container mx-auto ">
+      <div className="flex justify-between items-center gap-4 py-8">
+        <div></div>
+
+        <ul className="flex items-center justify-center gap-3.5 text-gray-600">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/career">Career</Link>
+          </li>
+        </ul>
+        <div className="flex justify-center items-center gap-3 ">
+          <Image
+            src={Avatar}
+            alt="Profile Image"
+            width={40}
+            height={40}
+            className="rounded-full"
+          />
+          <Link href={"/login"}>
+            <button className=" py-2 px-4 bg-gray-700  text-white rounded-lg cursor-pointer">
+              Login
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
