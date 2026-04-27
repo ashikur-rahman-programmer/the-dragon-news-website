b74618ac877473e679a6cdcf44eb1e4fa40329d2
ashik081 <freelancerashik81@gmail.com>
2026-04-27 20:43:38 +0600

    project ready to deploy

--- src/proxy.js
+++ src/proxy.js
@@ -14,7 +14,7 @@ export async function proxy(request) {
     return NextResponse.next();
   }
 
-  return NextResponse.redirect(new URL("/", request.url));
+  return NextResponse.redirect(new URL("/login", request.url));
 }
 
 export const config = {
